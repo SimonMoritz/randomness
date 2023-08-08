cf6d36a71cf2dad1f9cf0f4811856a4a7bec651a
Simon Moritz <simon.moritz@protonmail.com>
2023-08-09 01:37:20 +0200

    new tests more FileRetrieval impl

--- src/main/java/randomness/FileRetrievalStrategy.java
+++ src/main/java/randomness/FileRetrievalStrategy.java
@@ -1,16 +1,38 @@
 package randomness;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import framework.RetrievalStrategy;
 
 public class FileRetrievalStrategy implements RetrievalStrategy {
 
-    private final String fileName;
+    public final String fileName;
     public FileRetrievalStrategy(String fileName){
         this.fileName = fileName;
     }
+
+    public String getSubstringFromFile(int from, int to){
+        String subString;
+        try {
+            File f = new File(this.fileName);
+            Scanner sc = new Scanner(f);
+            String line = sc.nextLine();
+            subString = line.substring(from,to);
+        } catch (FileNotFoundException e){
+            System.out.println("file not found");
+            throw new RuntimeException();
+        }
+        return subString;
+    }
     @Override
     public int[] getBitsFromExternalRG(int amount) {
-        System.out.println(fileName);
-        return new int[12];
+        String stringBits = getSubstringFromFile(0,amount);
+        int[] bits = new int[stringBits.length()];
+        int i = 0;
+        for (char c : stringBits.toCharArray()){
+            bits[i] = ((int) c) - 48;
+            i++;
+        }
+        return bits;
     }
 }
--- src/test/java/TestRandomness.java
+++ src/test/java/TestRandomness.java
@@ -2,16 +2,43 @@ import framework.RetrievalStrategy;
 import org.junit.jupiter.api.Test;
 import randomness.Generator;
 import randomness.FileRetrievalStrategy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestRandomness {
     @Test
-    public void shouldBeRandom(){
-        RetrievalStrategy r = new FileRetrievalStrategy("myFile.txt");
+    public void shouldBeRandomBitsCorrectLength(){
+        RetrievalStrategy r = new FileRetrievalStrategy("src/main/java/randomTextFiles/2023-08-08.txt");
         Generator g = new Generator(r);
         int[] bits = g.getRandomBits(12);
         System.out.println("amount of bits are");
         System.out.println(bits.length);
         assert(bits.length == 12);
-        System.out.println("JA");
+    }
+    @Test
+    public void shouldBeBeFileName(){
+        FileRetrievalStrategy r = new FileRetrievalStrategy("src/main/java/randomTextFiles/2023-08-08.txt");
+        assert(r.fileName.equals( "src/main/java/randomTextFiles/2023-08-08.txt"));
+    }
+
+    @Test
+    public void shouldRetrieveBitsFromFile(){
+        FileRetrievalStrategy r = new FileRetrievalStrategy("src/main/java/randomTextFiles/2023-08-08.txt");
+        String bits = r.getSubstringFromFile(1,6);
+        System.out.println(bits);
+        assert (bits.equals("01001"));
+    }
+
+    @Test
+    public void shouldBeCorrectLengthOfBitArray(){
+        RetrievalStrategy r = new FileRetrievalStrategy("src/main/java/randomTextFiles/2023-08-08.txt");
+        int[] bits = r.getBitsFromExternalRG(23);
+        assert (bits.length == 23);
+    }
+
+    @Test
+    public void shouldThrowRuntimeErrorOnFileName(){
+        RetrievalStrategy r = new FileRetrievalStrategy("doesNotExist.txt");
+        //the second argument is a lambda function also an executable, this executable is then checked for runtime error
+        assertThrows(RuntimeException.class, () -> r.getBitsFromExternalRG(52));
     }
 }
